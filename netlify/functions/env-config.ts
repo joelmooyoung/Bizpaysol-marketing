a742a3c5215516ff37118b3c5fa51372ba51a8b9
joelmooyoung <joelmooyoung@gmail.com>
2025-12-02 23:51:33 -0600

    debug: Add logging to env-config function

--- netlify/functions/env-config.ts
+++ netlify/functions/env-config.ts
@@ -1,6 +1,8 @@
 export default async (req: any) => {
   const token = process.env.VITE_CHATWOOT_TOKEN || "";
   
+  console.log("env-config function called, token length:", token.length);
+  
   return new Response(`window.__CHATWOOT_TOKEN__ = "${token}";`, {
     status: 200,
     headers: {
